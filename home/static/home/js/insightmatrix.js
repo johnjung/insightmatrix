a742c271efa95840cd6bab24d1fbbde7a0a9e1d2
John Jung <jej@moss.lib.uchicago.edu>
2019-08-16 14:50:08 -0500

    Work in progress.

--- home/static/home/js/insightmatrix.js
+++ home/static/home/js/insightmatrix.js
@@ -1,38 +1,24 @@
 $(document).ready(function() {
+    /*
     $('#formset-wrapper').formset({
         addText: 'add family member',
         deleteText: 'remove',
         prefix: 'familymember_set'
     });
+    */
 
     // click to add a label...
     $('#add-item').click(function(e) {
-      // get the highest number in the set so far.
-      var max_n = 0;
-      $('#label_table').find('input').each(function() {
-        if ($(this)[0].hasAttribute('id')) {
-          var n = $(this).attr('id').match(/-[0-9]-/g);
-          n = parseInt(id[0].substring(1, id[0].length - 1));
-          if (n > max_n) {
-            max_n = n;
-          }
-        }
-      });
-    
-      // make a copy of a new row from the template.
-      var new_row = $('#item-tpl').find('tr').clone();
-      new_row.find('input').each(function() {
-        // update id.
-        if ($(this)[0].hasAttribute('id')) {
-          var id = $(this).attr('id').replace('nnnn', String(n));
-          $(this).attr('id', id);
-        }
-        // update name.
-        if ($(this)[0].hasAttribute('name')) {
-          var name = $(this).attr('name').replace('nnnn', String(n));
-          $(this).attr('name', name);
-        }
-      });
-      new_row.appendTo('#label_table');
-  });
+        e.preventDefault();
+
+        // get the highest number in the set so far.
+        var next_n = $('#label_table').find('tbody').find('tr').length;
+
+        console.log(next_n);
+
+        // make that new row. 
+        var new_tr_string = "<tr><td><input id='id_labels-nnnn-id' type='hidden' name='labels-nnnn-id'><input id='id_labels-nnnn-project' type='hidden' name='labels-nnnn-project'><input id='id_labels-nnnn-name' type='text' name='labels-nnnn-name' maxlength='200'></td><td><input type='checkbox' name='labels-2-DELETE' id='id_labels-2-DELETE'></td></tr>";
+        new_tr_string = new_tr_string.replace(/nnnn/g, String(next_n));
+        $(new_tr_string).appendTo($('#label_table').find('tbody'));
+    });
 });
