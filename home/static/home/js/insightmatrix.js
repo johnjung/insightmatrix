06482dbbd3fc020f10944ddc1404bde97310d3cf
John Jung <jej@moss.lib.uchicago.edu>
2019-08-16 12:54:53 -0500

    Work in progress.

--- home/static/home/js/insightmatrix.js
+++ home/static/home/js/insightmatrix.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.formset_row').formset({
+    $('#formset-wrapper').formset({
         addText: 'add family member',
         deleteText: 'remove',
         prefix: 'familymember_set'
