de1152e52e80d90c0b869b019b4171e24c3a2aaf
John Jung <john@fieldrotation.com>
2019-08-20 06:56:28 -0500

    First draft of code to find next random unranked label pair.

--- home/models.py
+++ home/models.py
@@ -1,7 +1,10 @@
+import itertools
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+
 
 class Project(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -15,12 +18,58 @@ class Project(models.Model):
     description = models.TextField(default='')
     creation_date = models.DateTimeField(default=timezone.now, editable=False)
 
+    def get_all_label_pairs(self, ranked=True, unranked=True):
+        """Should return pairs, e.g.:
+            [
+                ((1, 'apple'), (2, 'orange')),
+                ((1, 'apple'), (3, 'peach'))
+            ]
+        """
+
+        all_labels = set(
+            Label.objects.filter(project=self).values_list('pk', 'name')
+        )
+        ranked_labels = set(
+            Similarity.objects.filter(
+                label_one__project=self
+            ).values_list(
+                'label_one__pk',
+                'label_one__name'
+            )
+        ).union(
+            set(
+                Similarity.objects.filter(
+                    label_two__project=self
+                ).values_list(
+                    'label_two__pk',
+                    'label_two__name'
+                )
+            )
+        )
+        unranked_labels = all_labels.difference(ranked_labels)
+
+        if ranked and unranked:
+            return itertools.combinations(all_labels)
+        elif ranked:
+            return itertools.combinations(ranked_labels)
+        elif unranked:
+            return itertools.combinations(unranked_labels)
+        else:
+            return itertools.combinations(set())
+
+    def get_random_unranked_label_pair(self):
+        return random.shuffle(
+            self.get_all_label_pairs(ranked=False, unranked=True)
+        )[0]
+
+
 class Label(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="labels")
     name = models.CharField(max_length=200)
 
     class Meta:
         unique_together = ('project', 'name')
+
 
 class Similarity(models.Model):
     label_one = models.ForeignKey(Label, on_delete=models.CASCADE, related_name="similarity_one")
--- home/views.py
+++ home/views.py
@@ -67,12 +67,23 @@ def project_update(request, pk=None):
         }
     )
 
+def project_detail(request, pk=None):
+    # add a new similarity score to the database.
+    if request.method == "POST":
+        Similarity.objects.create(
+            label_one=Labels.objects.get(pk=int(request.POST['label_one'])),
+            label_two=Labels.objects.get(pk=int(request.POST['label_two'])),
+            score=int(request.POST['score'])
+        )
+
+    random_label_pair = Project.objects.get(pk=pk).get_random_unranked_label_pair()
+    if random_label_pair:
+        # display the pair form.
+        raise NotImplementedError
+    else:
+        # display the SVG.
+        raise NotImplementedError
     
-class ProjectDetail(LoginRequiredMixin, DetailView):
-    login_url = '/accounts/login/'
-    model = Project
-
-
 class ProjectList(LoginRequiredMixin, ListView):
     login_url = '/accounts/login/'
     model = Project
