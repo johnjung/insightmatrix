4afb25b397afb63a78745365d45d713475a39d8e
John Jung <john@fieldrotation.com>
2019-08-24 07:46:37 -0500

    Work on browser-based tests.

--- home/tests.py
+++ home/tests.py
@@ -1,13 +1,16 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-
-# https://medium.com/django-musings/integration-testing-on-django-1ac6a0c428c8
+import time
 
 class InsightMatrixFunctionalTests(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super(InsightMatrixFunctionalTests, cls).setUpClass()
-        cls.selenium = webdriver.Chrome()
+        cls.selenium = webdriver.Safari()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
 
     def test_user_signs_up_for_an_account(self):
         """When a user first goes to the site, they should see a short
@@ -17,24 +20,45 @@ class InsightMatrixFunctionalTests(StaticLiveServerTestCase):
         # A user visits the site homepage.
         self.selenium.get('{}/'.format(self.live_server_url))
 
+        # They see some helpful text telling them what this site is about.
         self.assertTrue("Insight Matrix is a tool for finding patterns" in self.selenium.page_source)
 
-        # Find the login button.
+        # They find the login button.
         self.selenium.find_element_by_link_text("Login to your account").click()
+        time.sleep(1)
 
-        # Try to login.
-        # But it fails.
+        # This user doesn't have an account, but tries to log in.
+        self.selenium.find_element_by_id("id_username").send_keys("nobody")
+        self.selenium.find_element_by_id("id_password").send_keys("nopassword")
+        self.selenium.find_element_by_tag_name("form").submit()
+        time.sleep(1)
+
+        # They get a message saying that their password is incorrect.
+        self.assertTrue("Please enter a correct username and password." in self.selenium.page_source)
 
         # Back to the homepage.
-        #self.selenium.get('{}/'.format(self.live_server_url))
+        self.selenium.get('{}/'.format(self.live_server_url))
+        time.sleep(1)
 
-        # Find the login button.
-        #self.selenium.find_element_by_link_text("sign up for a new account")
+        # Now the user clicks to sign up for a new account.
+        self.selenium.find_element_by_link_text("sign up for a new account").click()
+        time.sleep(1)
 
-        # Sign up for an account.
+        # The user fills out our form.
+        self.selenium.find_element_by_id("id_username").send_keys("somebody")
+        self.selenium.find_element_by_id("id_password1").send_keys("somebodyspassword")
+        self.selenium.find_element_by_id("id_password2").send_keys("somebodyspassword")
+        self.selenium.find_element_by_tag_name("form").submit()
+        time.sleep(1)
+
         # Now try logging in.
-        # It succeeds.
-        # They should land on the project list page, which will be empty.
+        self.selenium.find_element_by_id("id_username").send_keys("somebody")
+        self.selenium.find_element_by_id("id_password").send_keys("somebodyspassword")
+        self.selenium.find_element_by_tag_name("form").submit()
+        time.sleep(1)
+
+        # It succeeds. They get redirected to an empty project page.
+        self.assertTrue("There are currently no projects." in self.selenium.page_source)
 
     '''
     def test_user_creates_first_project(self):
